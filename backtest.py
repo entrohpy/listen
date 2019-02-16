25789d81f8a3010c74a883eb6c9c379741da4816
Nikhil Malhotra <nikhilmalhotra@dhcp-v098-090.mobile.uci.edu>
2019-02-16 15:17:59 -0800

    mostly working WordTimeMap

--- backtest.py
+++ backtest.py
@@ -1,6 +1,9 @@
 import io
 import os
 
+import requests
+import json
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
 # Imports the Google Cloud client library
@@ -8,41 +11,49 @@ from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
-# Instantiates a client
-client = speech.SpeechClient()
+def returnWordTimeMap(audiofile): #currently doesn't work
+    # Instantiates a client
+    client = speech.SpeechClient()
+    #Start converting file to mono flac
+    os.system("ftransc -f flac " + audiofile)
+    os.system("ffmpeg -i " + audiofile + " -ac 1 " + audiofile[:len(audiofile) - 5] + "mono.flac"
+    #End converting file
 
-uri = 'gs://test75829/1min.ogg'
-audio = {'uri': uri}
+    uri = 'gs://test75829/9min.flac'
+    #uri = 'gs://python-docs-samples-tests/speech/audio.flac'
+    audio = {'uri': uri}
 
-config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.OGG_OPUS,
-    sample_rate_hertz=48000,
-    language_code='en-US',
-    enable_word_time_offsets=True)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=44100,
+        language_code='en-US',
+        enable_word_time_offsets=True)
 
-#New stuff
-operation = client.long_running_recognize(config, audio)
-print('Waiting for operation to complete...')
-response = operation.result(timeout=60) #Set to something
-
-# Each result is for a consecutive portion of the audio. Iterate through
-# them to get the transcripts for the entire audio file.
-for result in response.results:
-    # The first alternative is the most likely one for this portion.
-    alternative = result.alternatives[0]
-    print(u'Transcript: {}'.format(result.alternatives[0].transcript))
-    print('Confidence: {}'.format(result.alternatives[0].confidence))
-    for word_info in alternative.words:
-            word = word_info.word
-            start_time = word_info.start_time
-            end_time = word_info.end_time
-            print('Word: {}, start_time: {}, end_time: {}'.format(
-                word,
-                start_time.seconds + start_time.nanos * 1e-9,
-                end_time.seconds + end_time.nanos * 1e-9))
-
-# Detects speech in the audio file OLD
-# response = client.recognize(config, audio)
-# print (response)
-# for result in response.results:
-#     print('Transcript: {}'.format(result.alternatives[0].transcript))
+    #New stuff
+    operation = client.long_running_recognize(config, audio)
+    print('Waiting for operation to complete...')
+    response = operation.result() #Set to something
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    wordTimeMap = []
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        alternative = result.alternatives[0]
+        #print(u'Transcript: {}'.format(result.alternatives[0].transcript))
+        #print('Confidence: {}'.format(result.alternatives[0].confidence))
+        for word_info in alternative.words:
+            wordTimeMap.append((word_info.word, word_info.start_time))
+            # word = word_info.word
+            # start_time = word_info.start_time
+            # end_time = word_info.end_time
+            # print('Word: {}, start_time: {}, end_time: {}'.format(
+            #     word,
+            #     start_time.seconds + start_time.nanos * 1e-9,
+            #     end_time.seconds + end_time.nanos * 1e-9))
+    for key, value in wordTimeMap:
+        print ("Word: " + str(key) + "\t\t'Start Time: " + str(value.seconds + value.nanos * 1e-9))
+    # Detects speech in the audio file OLD
+    # response = client.recognize(config, audio)
+    # print (response)
+    # for result in response.results:
+    #     print('Transcript: {}'.format(result.alternatives[0].transcript))
